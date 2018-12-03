a1f057b8856dcbfdf300b6d3f9a61e4c7cd1ea4f
Sushant Shrestha <yala.sushantshrestha.com>
2018-12-03 11:40:40 +0545

    Box collision fixed

--- Ast-2/js/box.js
+++ Ast-2/js/box.js
@@ -1,6 +1,5 @@
 console.log('box.js');
 
-
 var boxArray = [];
 var colorArray = ['red', 'yellow', 'blue', 'white', 'black'];
 
@@ -16,9 +15,8 @@ function Box(x, y) {
   this.color = colorArray[c];
   this.element;
 
-  this.create = function (parent) {
+  this.create = function(parent) {
     this.element = document.createElement('div');
-
     this.element.style.width = this.width;
     this.element.style.height = this.height;
     this.element.style.position = 'absolute';
@@ -27,98 +25,100 @@ function Box(x, y) {
     parent.appendChild(this.element);
   }
 
-  this.setPosition = function (x, y) {
+  this.setPosition = function(x, y) {
     this.x = x;
     this.y = y;
     this.element.style.top = this.y + 'px';
     this.element.style.left = this.x + 'px';
   }
 
-  this.move = function () {
+  this.move = function() {
     this.x = this.x + this.dirX;
     this.y = this.y + this.dirY;
   }
 
-  this.update = function () {
+  this.update = function() {
     this.element.style.top = this.y + 'px';
     this.element.style.left = this.x + 'px';
   }
 
-  this.checkCollision = function (mainContainer) {
+  this.checkWallCollision = function(mainContainer) {
 
-    console.log(mainContainer.style.height);
-
+    // console.log(mainContainer.style.height);
     if ((this.x < 0) || ((this.x + this.width > parseInt(mainContainer.style.width, 10)))) {
       this.dirX = this.dirX * -1;
-      console.log('collision x');
+      // console.log('collision x');
     }
 
     if ((this.y < 0) || ((this.y + this.height > parseInt(mainContainer.style.height, 10)))) {
       this.dirY = this.dirY * -1;
-      console.log('collision y');
+      // console.log('collision y');
     }
   }
 
-  this.checkObjCollision = function () {
+  this.checkObjCollision = function(boxArray) {
     var box1, box2;
+    box1 = this;
 
     for (var i = 0; i < boxArray.length; i++) {
-      for (var j = 0; j < boxArray.length; j++) {
-        if (i != j) {
-          box1 = boxArray[i];
-          box2 = boxArray[j];
-          if (box1.x < box2.x + box2.width &&
-            box1.x + box1.width > box2.x && box1.y < box2.y + box2.height &&
-            box1.height + box1.y > box2.y) {
-            box1.dirY *= 1;
-            box2.dirY *= -1;
-            box1.dirX *= -1;
-            box2.dirX *= 1;
-          }
+      box2 = boxArray[i];
+      if (boxArray[i] != this) {
+        if (box1.x < box2.x + box2.width &&
+          box1.x + box1.width > box2.x && box1.y < box2.y + box2.height &&
+          box1.height + box1.y > box2.y) {
+          // console.log('collisionDetected');
+
+          var tempx, tempy;
+          tempx = box1.dirX;
+          tempy = box1.dirY;
+
+          box1.dirX = box2.dirX;
+          box1.dirY = box2.dirY;
+
+          box2.dirX = tempx;
+          box2.dirY = tempy;
+
         }
       }
     }
   }
 }
 
-
 function init() {
 
   var width = 200;
   var height = 200;
+  var boxNos = 10;
+
   var mainContainer = document.getElementById('main-container');
   mainContainer.style.width = width + 'px';
   mainContainer.style.height = height + 'px';
   mainContainer.style.backgroundColor = 'green';
 
-  var boxNos = 10;
-
+  
   for (var i = 0; i < boxNos; i++) {
     var box = new Box(generateRandomNumber(width - 20), generateRandomNumber(height - 20));
     box.create(mainContainer);
     boxArray.push(box);
   }
 
-
-  var mainInterval = setInterval(function () {
+  var mainInterval = setInterval(function() {
     for (var i = 0; i < boxArray.length; i++) {
 
       boxArray[i].move();
 
       boxArray[i].update();
 
-      boxArray[i].checkCollision(mainContainer);
+      boxArray[i].checkWallCollision(mainContainer);
 
-      boxArray[i].checkObjCollision();
-
+      boxArray[i].checkObjCollision(boxArray);
 
     }
-   
+
   }, 1000 / 30);
 }
 
 init();
-
 
 
 /**
@@ -128,5 +128,4 @@ init();
  */
 function generateRandomNumber(x) {
   return Math.floor((Math.random() * x) + 1);
-
 }
